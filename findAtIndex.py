0c81ede3e05710429be8f3399193e3d940c56f83
Ness Mayker <nessmayker@gmail.com>
2021-10-02 13:29:51 -0400

    added binary searching algorithm to speed up finding closest value search

--- findAtIndex.py
+++ findAtIndex.py
@@ -1,3 +1,7 @@
+
+import numpy as np
+
+
 def atIndex(lst, K, vals): 
     '''
     Returns the value in an second array at the index that is closest to the value requested.
@@ -19,7 +23,7 @@ def atIndex(lst, K, vals):
 	cdf : array
 		CDF array 
     '''
-    import numpy as np
+
 
     absVal = []
     
@@ -30,3 +34,53 @@ def atIndex(lst, K, vals):
     index = absVal.index(value)
     want  = vals[index]
     return(want)
+
+
+
+def getClosest(val1, val2, target):
+    if (target - val1 >= val2 - target):
+        return(val2)
+    else:
+        return(val1)            
+ 
+# Returns value closest to target value in sorted array
+def findClosest(array, target):
+ 
+    #Edge cases
+    if (target <= array[0]):
+        return(array[0], 0)
+
+    if (target >= array[-1]):
+        return(array[-1], len(array))
+ 
+    #Binary search
+    i, j, mid = 0, len(array), 0
+    
+    while (i < j):
+        mid = (i + j) // 2
+ 
+        if (array[mid] == target):
+            return(array[mid], np.where(array==target))
+ 
+        # If target is less than middle value, search on left side
+        if (target < array[mid]) :
+ 
+            # If target is greater than the previous to middle, return closest of the two
+            if (mid > 0 and target > array[mid - 1]):
+                value = getClosest(array[mid - 1], array[mid], target)
+                return(value, np.where(array==value))
+ 
+            # Repeat for right half
+            j = mid
+         
+        # If target is greater than mid
+        else :
+            if (mid < len(array) - 1 and target < array[mid + 1]):
+                value = getClosest(array[mid], array[mid + 1], target)
+                return(value, np.where(array==value))
+                 
+            # update i
+            i = mid + 1
+         
+    # Only a single value remains after search
+    return (array[mid], np.where(array==target))
